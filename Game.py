7774a43873ba4a30c04b5526eb3ab1fd052deeec
David Ionita <31045833+davidionita@users.noreply.github.com>
2021-10-15 20:27:47 +0000

    nothing really

--- Game.py
+++ Game.py
@@ -1,6 +1,6 @@
 # David Ionita & Ethan Platock
-# Project 2 Phase 1
-# 10/04/2021
+# Project 2 Phase 2
+# 10/08/2021
 # Professor Dancy
 
 """ Game to play 'Lost Rovers'. This is the file you edit.
@@ -18,50 +18,9 @@ class Game:
 
         self.rover = Rover(Point(randint(0, Game.SIZE-1), randint(0, Game.SIZE-1)))
 
-        self.room = Room(Game.SIZE)
+        self.rooms = [Room(Game.SIZE, True)] # acts as a stack to push(append), pop, and peak([-1]) rooms
         
-        # Hardcoded ship components
-        self.room.set_location(Point(6,6), ShipComponent(Point(6,6), "cabinbroken.ppm", "cabin", True))     # broken cabin @ 6,6
-        self.room.set_location(Point(7,6), ShipComponent(Point(7,6), "enginebroken.ppm", "engine", True))   # broken engine @ 7,6
-        self.room.set_location(Point(6,7), ShipComponent(Point(6,7), "cabin.ppm", "cabin", False))          # fixed cabin @ 6,7
-        self.room.set_location(Point(7,7), ShipComponent(Point(7,7), "engine.ppm", "engine", False))        # fixed engine @ 7,7
-        self.room.set_location(Point(8,7), ShipComponent(Point(8,7), "exhaustbroken.ppm", "exhaust", True)) # broken exhaust @ 8,7
-        self.room.set_location(Point(8,8), ShipComponent(Point(8,8), "cabinbroken.ppm", "cabin", True))     # broken cabin @ 8,8
-         
-        # Random number of portals from 2 to 6
-        numPortals = randint(2, 6)
-        for i in range(numPortals):
-          # Choose random location for portal without a current item on it
-          position = Point(randint(0, Game.SIZE-1), randint(0, Game.SIZE-1))
-          while self.room.get_location(position) is not None:
-            position = Point(randint(0, Game.SIZE-1), randint(0, Game.SIZE-1)) 
-          self.room.set_location(position, Portal(position))
-
-        # Static list of parts for easy indexing w/ randint
-        PARTS = ["bagel", "cake", "lettuce", "gear", "screw"]
-        # Helper list to keep track of distinct parts (need at least 4 different parts)
-        distinctParts = [None, None, None, None]
-        # Random number of parts from 7 to 12
-        numParts = randint(7, 12)
-        for i in range(numParts):
-          # Choose random location for part without a current item on it
-          position = Point(randint(0, Game.SIZE-1), randint(0, Game.SIZE-1))
-          while self.room.get_location(position) is not None:
-            position = Point(randint(0, Game.SIZE-1), randint(0, Game.SIZE-1))
-          
-          # Random part (0-4 as indexed in the static list)
-          kindNum = randint(0, 4)
-          if i<4: # For the first 4 parts, make sure they're distinct; THEN randomize the kind/type for rest of them
-            kindNum = randint(0, 4)
-            # Make sure randomly chosen part is distinct
-            while kindNum in distinctParts:
-              kindNum = randint(0, 4)
-            distinctParts[i] = kindNum
-            self.room.set_location(position, Part(position, PARTS[kindNum]+".ppm", PARTS[kindNum]))
-          else:
-            kindNum = randint(0, 4)
-            self.room.set_location(position, Part(position, PARTS[kindNum]+".ppm", PARTS[kindNum]))
-
+        self.current_room = self.rooms[-1] # current_room tracks the room the rover is in and displayed on the screen
 
     def start_game(self):
         self.gui.run()
@@ -83,7 +42,7 @@ class Game:
 		part, ship component, or portal at the given
 		coordinates. ('engine.ppm' or 'cake.ppm' or
 		'portal.ppm', etc) """
-        item = self.room.get_location(point)
+        item = self.current_room.get_location(point)
         
         if item is None:
             return None
@@ -98,6 +57,9 @@ class Game:
           print("Hit wall, cannot move up")
         else:
           self.rover.position.y -= 1
+          new_location = self.current_room.get_location(self.rover.position)
+          if isinstance(new_location, Portal): self.teleport(new_location)
+
 
     def go_down(self):
         """ Called by GUI when button clicked.
@@ -107,6 +69,8 @@ class Game:
           print("Hit wall, cannot move down")
         else:
           self.rover.position.y += 1
+          new_location = self.current_room.get_location(self.rover.position)
+          if isinstance(new_location, Portal): self.teleport(new_location)
 
     def go_left(self):
         """ Called by GUI when button clicked.
@@ -116,6 +80,8 @@ class Game:
           print("Hit wall, cannot move left")
         else:
           self.rover.position.x -= 1
+          new_location = self.current_room.get_location(self.rover.position)
+          if isinstance(new_location, Portal): self.teleport(new_location)
 
     def go_right(self):
         """ Called by GUI when button clicked.
@@ -125,6 +91,33 @@ class Game:
           print("Hit wall, cannot move right")
         else:
           self.rover.position.x += 1
+          new_location = self.current_room.get_location(self.rover.position)
+          if isinstance(new_location, Portal): self.teleport(new_location)
+
+    def teleport(self, portal):
+      """ Called by game when rover stands on portal.
+      If rover is standing on a a portal, travel to 
+      connecting room.  Portal is only linked between
+      two rooms.
+      """
+      #if old and have NO new, then create new and go to it
+      if portal.current == "old" and portal.room_new is None:
+        portal.room_new = Room(Game.SIZE, False, portal)
+        self.rooms.append(portal.room_new)
+        self.current_room = self.rooms[-1]
+        portal.current = "new"
+        
+      #elif old and have new, then go to it
+      elif portal.current == "old" and portal.room_new is not None:
+        self.rooms.append(portal.room_new)
+        self.current_room = self.rooms[-1]
+        portal.current = "new"
+
+      #elif new, then go back to old
+      else:
+        self.rooms.pop()
+        self.current_room = self.rooms[-1]
+        portal.current = "old"
 
     def show_way_back(self):
         """ Called by GUI when button clicked.
@@ -177,8 +170,11 @@ class StaticItem:
     self.image = image
 
 class Portal(StaticItem):
-  def __init__(self, position, image="portal.ppm"):
+  def __init__(self, position, image, init_room):
     super().__init__(position, image)
+    self.room_old = init_room
+    self.room_new = None
+    self.current = "old"
 
 class Part(StaticItem):
   def __init__(self, position, image, kind):
@@ -192,12 +188,65 @@ class ShipComponent(StaticItem):
     self.broken = broken
 
 class Room:
-  def __init__(self, size):
+  def __init__(self, size, first, portal_back=None):
     self.size = size
     
     # Use list comprehension to make a 2D array full of None
     self.board = [[None for x in range(size)] for x in range(size)]
     # Board can be indexed by [Point.y][Point.x] because it is [row, column]
+
+    if first:
+      # Hardcoded ship components
+      self.set_location(Point(6,6), ShipComponent(Point(6,6), "cabinbroken.ppm", "cabin", True))     # broken cabin @ 6,6
+      self.set_location(Point(7,6), ShipComponent(Point(7,6), "enginebroken.ppm", "engine", True))   # broken engine @ 7,6
+      self.set_location(Point(6,7), ShipComponent(Point(6,7), "cabin.ppm", "cabin", False))          # fixed cabin @ 6,7
+      self.set_location(Point(7,7), ShipComponent(Point(7,7), "engine.ppm", "engine", False))        # fixed engine @ 7,7
+      self.set_location(Point(8,7), ShipComponent(Point(8,7), "exhaustbroken.ppm", "exhaust", True)) # broken exhaust @ 8,7
+      self.set_location(Point(8,8), ShipComponent(Point(8,8), "cabinbroken.ppm", "cabin", True))     # broken cabin @ 8,8
+
+      # Random number of portals from 2 to 6
+      numPortals = randint(2, 6)
+      
+    else:
+      # Set the OG portal location to make sure it has a spot
+      self.set_location(portal_back.position, portal_back)
+
+      # Random number of portals from 2 to 5
+      numPortals = randint(2, 5)
+      pass
+    
+    
+    for i in range(numPortals):
+      # Choose random location for portal without a current item on it
+      position = Point(randint(0, self.size-1), randint(0, self.size-1))
+      while self.get_location(position) is not None:
+        position = Point(randint(0, self.size-1), randint(0, self.size-1)) 
+      self.set_location(position, Portal(position, "portal.ppm", self))
+
+    # Static list of parts for easy indexing w/ randint
+    PARTS = ["bagel", "cake", "lettuce", "gear", "screw"]
+    # Helper list to keep track of distinct parts (need at least 4 different parts)
+    distinctParts = [None, None, None, None]
+    # Random number of parts from 7 to 12
+    numParts = randint(7, 12)
+    for i in range(numParts):
+      # Choose random location for part without a current item on it
+      position = Point(randint(0, self.size-1), randint(0, self.size-1))
+      while self.get_location(position) is not None:
+        position = Point(randint(0, self.size-1), randint(0, self.size-1))
+      
+      # Random part (0-4 as indexed in the static list)
+      kindNum = randint(0, 4)
+      if i<4: # For the first 4 parts, make sure they're distinct; THEN randomize the kind/type for rest of them
+        kindNum = randint(0, 4)
+        # Make sure randomly chosen part is distinct
+        while kindNum in distinctParts:
+          kindNum = randint(0, 4)
+        distinctParts[i] = kindNum
+        self.set_location(position, Part(position, PARTS[kindNum]+".ppm", PARTS[kindNum]))
+      else:
+        kindNum = randint(0, 4)
+        self.set_location(position, Part(position, PARTS[kindNum]+".ppm", PARTS[kindNum]))    
 
   def get_location(self, position):
     return self.board[position.y][position.x]
